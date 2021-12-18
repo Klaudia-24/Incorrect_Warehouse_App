7d22b6f8a06618bf1a82d5976df42805895cb179
Klaudia-24 <klaudia.winer@gmail.com>
2021-12-19 00:19:10 +0100

    Added style of the login activity. Added background images and other icons.

--- app/src/main/java/com/example/incorrect_warehouse_app/activity/LoginActivity.kt
+++ app/src/main/java/com/example/incorrect_warehouse_app/activity/LoginActivity.kt
@@ -1,18 +1,19 @@
-package com.example.incorrect_warehouse_app
+package com.example.incorrect_warehouse_app.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.example.incorrect_warehouse_app.R
 import com.example.incorrect_warehouse_app.model.PostgreSqlDatabase
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     companion object {
         val db = PostgreSqlDatabase()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
 
         Log.d("TEST","Proba polaczenia")
         db.dbConn("try")
