e64b92dd13cba07c187987a5ad0bec2dfcbf943f
Klaudia-24 <klaudia.winer@gmail.com>
2021-12-19 01:17:41 +0100

    Added navigation activity v2.

---
+++ app/src/main/java/com/example/incorrect_warehouse_app/activity/NavigationActivity.kt
@@ -0,0 +1,12 @@
+package com.example.incorrect_warehouse_app.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.incorrect_warehouse_app.R
+
+class NavigationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_navigation)
+    }
+}
