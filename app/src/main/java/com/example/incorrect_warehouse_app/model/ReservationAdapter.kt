1e83f98713942fb2dfac79cd29c2f5720c6254e0
Klaudia-24 <klaudia.winer@gmail.com>
2022-01-07 00:13:46 +0100

    Added adapters and lists for reservations and employees part 2.

--- app/src/main/java/com/example/incorrect_warehouse_app/model/ReservationAdapter.kt
+++ app/src/main/java/com/example/incorrect_warehouse_app/model/ReservationAdapter.kt
@@ -1,4 +1,60 @@
 package com.example.incorrect_warehouse_app.model
 
-class ReservationAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.incorrect_warehouse_app.R
+import com.google.android.material.textview.MaterialTextView
+import java.util.*
+
+class ReservationAdapter(private var reservationList: List<Reservation>):
+    RecyclerView.Adapter<ReservationAdapter.ViewHolder>()  {
+
+    private lateinit var mListner: onItemClickListner
+
+    interface onItemClickListner{
+        fun onItemClick(position: Int)
+    }
+
+    fun setOnItemClickListener(listener: onItemClickListner){
+        mListner = listener
+    }
+
+    inner class ViewHolder(reservationView: View, listener: onItemClickListner): RecyclerView.ViewHolder(reservationView){
+        val productName: MaterialTextView = reservationView.findViewById(R.id.resProductName)
+        val productSize: MaterialTextView = reservationView.findViewById(R.id.resProductSize)
+        val productAmount: MaterialTextView = reservationView.findViewById(R.id.resProductAmount)
+        val productPrice: MaterialTextView = reservationView.findViewById(R.id.resProductPrice)
+        val reservationDate: MaterialTextView = reservationView.findViewById(R.id.resDate)
+        val employeeName: MaterialTextView = reservationView.findViewById(R.id.resEmployeeName)
+        val employeeSurname: MaterialTextView = reservationView.findViewById(R.id.resEmployeeSurname)
+
+        init {
+            reservationView.setOnClickListener {
+                listener.onItemClick(adapterPosition)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.reservation_list, parent, false)
+        return ViewHolder(view, mListner)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.productName.text = reservationList[position].name
+        holder.productSize.text = reservationList[position].sizeofproduct.toString()
+        holder.productAmount.text = reservationList[position].amount.toString()
+        holder.productPrice.text = reservationList[position].price.toString() + " " + java.util.Currency.getInstance("GBP").getSymbol(
+            Locale.ENGLISH
+        )
+        holder.reservationDate.text = reservationList[position].reservationdate
+        holder.employeeName.text = reservationList[position].employeename
+        holder.employeeSurname.text = reservationList[position].employeesurname
+    }
+
+    override fun getItemCount(): Int {
+        return reservationList.size
+    }
 }
