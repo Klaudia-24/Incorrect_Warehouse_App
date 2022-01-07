2a385885f38ad1b998e8ef21a5b3fcc35e21da1b
Klaudia-24 <klaudia.winer@gmail.com>
2022-01-07 18:31:30 +0100

    Added classes and dialog windows for admin employee data.

---
+++ app/src/main/java/com/example/incorrect_warehouse_app/model/EmployeeAdminData.kt
@@ -0,0 +1,25 @@
+package com.example.incorrect_warehouse_app.model
+
+data class EmployeeAdminData(
+
+    val employeeid: Int,
+    val name: String,
+    val surname: String,
+    val login: String,
+    val email: String,
+    val rolename: String
+)
+
+//    employeeId serial PRIMARY KEY,
+//    login VARCHAR ( 40 ) NOT NULL,
+//    password VARCHAR ( 100 ) NOT NULL,
+//    email VARCHAR ( 50 ) NOT NULL,
+
+
+
+//employeeId serial PRIMARY KEY,
+//name VARCHAR ( 30 ) NOT NULL,
+//surname VARCHAR ( 40 ) NOT NULL,
+//login VARCHAR ( 40 ) NOT NULL,
+//email VARCHAR ( 50 ) NOT NULL,
+//roleName VARCHAR ( 30 ) NOT NULL
